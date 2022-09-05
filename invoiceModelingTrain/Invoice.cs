54f9cfb538f90ae42278e019db9574962b626905
Deem <Deem@DEEM>
2022-09-05 12:10:15 +0300

    Добавьте файлы проекта.

---
+++ invoiceModelingTrain/Invoice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invoiceModelingTrain
+{
+    internal class Invoice
+    {
+        // title
+        public DateTime Date { get; init; }
+        public string Number { get; init; } = null;
+        public string To { get; init; } = null;
+        public string From  { get; init; } = null;
+
+        // body
+
+
+        
+        // footer
+        public decimal Total { get; private set; }
+        public string StorekeeperSurname { get; init; } = null;
+        public string ForwarderSurname { get; init; } = null;
+    }
+
+}
